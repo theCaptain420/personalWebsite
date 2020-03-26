df6864c63124b34f1a2f142935a50989b8055f50
thecaptain420 <mads.nielsen@hotmail.com>
2020-03-26 16:24:24 +0100

    added css to main component

--- personal-website/src/Components/welcomeMod/welcome.tsx
+++ personal-website/src/Components/welcomeMod/welcome.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './welcome.css'
 
 export interface IWelcomeIntroProps {
 }
@@ -6,10 +7,14 @@ export interface IWelcomeIntroProps {
 export default class WelcomeIntro extends React.Component<IWelcomeIntroProps> {
   public render() {
     return (
-      <div>
-        <p>
+      <div className="header">
+        <p className="headerText">
             Welcome to my page
+        </p> 
+        <p className="normalText">
+          hello
         </p>
+      
       </div>
     );
   }
