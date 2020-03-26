bfcd7c68ad9cb4d3be207c3e2b5f9161be9cd0b4
thecaptain420 <mads.nielsen@hotmail.com>
2020-03-26 16:24:12 +0100

    adjusted page, and removed unnessecary

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -8,24 +8,7 @@ function App() {
   return (
     <div className="App">
       <WelcomeModule></WelcomeModule>
-      <header className="App-header">
         
-        
-        <p>
-          Edit  and save to reload.
-        </p>
-
-
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
