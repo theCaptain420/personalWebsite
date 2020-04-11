4fa949f103f42e9211b83d0cc8a8abcdedf68c32
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-11 11:16:54 +0200

    HEADER

---
+++ personal-website/src/Components/themeComponents/WebHeader.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './themeStyle.css'
+
+export default ()=>{
+    return(
+        <div className="headerBackground">
+            <p className="headerText">Mads' side</p>
+        </div>
+    )
+}
