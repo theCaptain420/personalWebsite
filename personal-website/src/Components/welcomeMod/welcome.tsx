2af6baa48fcf7c4de8836a221d1546b3173fd262
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-11 11:20:40 +0200

    changed page style and added themecomponents

--- personal-website/src/Components/welcomeMod/welcome.tsx
+++ personal-website/src/Components/welcomeMod/welcome.tsx
@@ -1,20 +1,24 @@
 import * as React from 'react';
 import './welcome.css'
-
+import WebHeader from '../themeComponents/WebHeader'
+import WebFooter from '../themeComponents/WebFooter'
 export interface IWelcomeIntroProps {
 }
 
 export default class WelcomeIntro extends React.Component<IWelcomeIntroProps> {
   public render() {
     return (
-      <div className="header">
+      <div className="wholePage">
+        <WebHeader/>
         <p className="headerText">
             Welcome to my page
         </p> 
+
         <p className="normalText">
           hello
         </p>
-      
+
+        <WebFooter/>
       </div>
     );
   }
