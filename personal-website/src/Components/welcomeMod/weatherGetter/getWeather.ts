7fe739c171d217fda96638e003c0c8df5de07ed0
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-13 20:25:05 +0200

    setup for real sceniario

--- personal-website/src/Components/welcomeMod/weatherGetter/getWeather.ts
+++ personal-website/src/Components/welcomeMod/weatherGetter/getWeather.ts
@@ -1,12 +1,12 @@
-export function getWeather(){
+export async function getWeather(){
     let url: string = "https://www.metaweather.com/api/location/554890/"
-    let responseTemp = fetch(url).then(resp=>resp.json()).then(respJSON => console.log(respJSON))
+    let responseTemp = await fetch(url).then(resp=>resp.json()).then(respJSON => respJSON.the_temp)
     
     console.log(responseTemp)
     //let tempPrint = responseTemp.toString()
     //console.log(tempPrint)
     
-   return "te"
+   return responseTemp
 
 
 }
