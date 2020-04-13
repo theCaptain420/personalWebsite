e8efa8b66dc657a374fa03830a1219ee46085fd2
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-13 20:25:21 +0200

    needs to fix cors problems

--- personal-website/src/Components/welcomeMod/weatherGetter/WeatherPrinter.tsx
+++ personal-website/src/Components/welcomeMod/weatherGetter/WeatherPrinter.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
 import {getWeather} from './getWeather'
 
-export default ()=>{
-     const currentWeather = getWeather();  
-
-
-    return(
-        <div>
-            <p>
-                {currentWeather}  
-            </p>
-        </div>
-    )
+type WeatherState = {
+    temp:string
 }
+
+export default class WeatherPrinter extends React.Component<{},WeatherState> {
+    async printTheWeather(){
+        try{
+            const temperature = getWeather();
+            await temperature.then(tempData => this.setState({temp:tempData}))
+        }catch(err){
+            console.log(err)
+            this.setState({temp:"temperature couldnt be gotten"})
+        }
+    }
+
+    componentWillMount(){
+        this.setState({temp:"lol"})
+        this.printTheWeather();
+    }
+
+    render() {
+    return (
+      <div >
+          {this.state.temp} degrees celcius
+      </div>
+    );
+  }
+}
+
