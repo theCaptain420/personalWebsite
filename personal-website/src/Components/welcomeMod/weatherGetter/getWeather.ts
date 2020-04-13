911b550674a8ed6053ff12713ec071a3ce807614
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-13 20:37:52 +0200

    cant get cors to work, feelsbad

--- personal-website/src/Components/welcomeMod/weatherGetter/getWeather.ts
+++ personal-website/src/Components/welcomeMod/weatherGetter/getWeather.ts
@@ -1,12 +1,18 @@
-export async function getWeather(){
+export async function getWeather() {
     let url: string = "https://www.metaweather.com/api/location/554890/"
-    let responseTemp = await fetch(url).then(resp=>resp.json()).then(respJSON => respJSON.the_temp)
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
     
+    let responseTemp = await fetch(url,{
+        headers:{
+            "Access-Control-Allow-Origin": "*"
+        }
+    }).then(resp => resp.json()).then(respJSON => respJSON.the_temp)
+
     console.log(responseTemp)
     //let tempPrint = responseTemp.toString()
     //console.log(tempPrint)
-    
-   return responseTemp
+
+    return responseTemp
 
 
 }
