8c390b76fc515bd8db4024725d7d5c284189b49b
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-15 20:17:59 +0200

    setup projects

---
+++ personal-website/src/Components/welcomeMod/projectElement/Project.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import ProjectGithub from './ProjectGithub'
+
+export default ()=>{
+    return(
+        <div>
+            <p>
+                Hey
+            </p>
+            
+        </div>
+    )
+}
