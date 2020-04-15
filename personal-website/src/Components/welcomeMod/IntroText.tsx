c0a1243871ad4d1dd180383da6d3bd052943bc9b
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-15 20:06:53 +0200

    text component

---
+++ personal-website/src/Components/welcomeMod/IntroText.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './welcome.css'
+
+export default()=>{
+    return(
+        <div>
+            <p className="wNormalText">
+                This page will show you all my mayor projects and applications <br/> that I've made throughout my years as a developer.
+            </p>
+        </div>
+    )
+}
