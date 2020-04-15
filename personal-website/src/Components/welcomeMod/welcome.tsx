ff13affa1ecf82c2b7d0f64de6c1e029c532f264
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-15 20:19:43 +0200

    implementation of project

--- personal-website/src/Components/welcomeMod/welcome.tsx
+++ personal-website/src/Components/welcomeMod/welcome.tsx
@@ -4,6 +4,7 @@ import WebHeader from '../themeComponents/WebHeader'
 import WebFooter from '../themeComponents/WebFooter'
 import WeatherPrinter from './weatherGetter/WeatherPrinter'
 import IntroText from './IntroText'
+import Project from './projectElement/Project'
 
 export interface IWelcomeIntroProps {
 }
@@ -21,6 +22,7 @@ export default class WelcomeIntro extends React.Component<IWelcomeIntroProps> {
           <WeatherPrinter/>
         </p>
         <IntroText></IntroText>
+        <Project/>
         <WebFooter/>
       </div>
     );
