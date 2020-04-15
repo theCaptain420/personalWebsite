3b9b13236e17ea6876845e5e2ecbbf416827c40f
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-15 11:13:31 +0200

    finally can print the temp

--- personal-website/src/Components/welcomeMod/weatherGetter/getWeather.ts
+++ personal-website/src/Components/welcomeMod/weatherGetter/getWeather.ts
@@ -2,17 +2,15 @@ export async function getWeather() {
     let url: string = "https://www.metaweather.com/api/location/554890/"
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     
-    let responseTemps = await fetch(proxyurl+url,{
+    let responseTemp = await fetch(proxyurl+url,{
         headers:{
             'Content-Type': 'application/json',
             "XMLHttpRequest":"X-Requested-With"
         }
-    }).then(resp => resp.json()).then(respJSON => respJSON)
+    }).then(resp => resp.json()).then(respJSON => respJSON.consolidated_weather[0].the_temp)
 
-    console.log(responseTemps)
     //let tempPrint = responseTemp.toString()
     //console.log(tempPrint)
-    let responseTemp="42"
     return responseTemp
 
 
