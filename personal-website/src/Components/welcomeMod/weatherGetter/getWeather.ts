98a95134601f649bb6f370a7172d4d6e60cdaf9c
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-15 11:09:57 +0200

    finally fixed CORS with proxy url

--- personal-website/src/Components/welcomeMod/weatherGetter/getWeather.ts
+++ personal-website/src/Components/welcomeMod/weatherGetter/getWeather.ts
@@ -2,16 +2,17 @@ export async function getWeather() {
     let url: string = "https://www.metaweather.com/api/location/554890/"
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     
-    let responseTemp = await fetch(url,{
+    let responseTemps = await fetch(proxyurl+url,{
         headers:{
-            "Access-Control-Allow-Origin": "*"
+            'Content-Type': 'application/json',
+            "XMLHttpRequest":"X-Requested-With"
         }
-    }).then(resp => resp.json()).then(respJSON => respJSON.the_temp)
+    }).then(resp => resp.json()).then(respJSON => respJSON)
 
-    console.log(responseTemp)
+    console.log(responseTemps)
     //let tempPrint = responseTemp.toString()
     //console.log(tempPrint)
-
+    let responseTemp="42"
     return responseTemp
 
 
