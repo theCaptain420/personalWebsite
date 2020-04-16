8469e43317d52483672fd9cd8c45daa4bc7f40bb
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-16 17:00:17 +0200

    made test array and test props

--- personal-website/src/Components/welcomeMod/projectElement/ProjectPrinter.tsx
+++ personal-website/src/Components/welcomeMod/projectElement/ProjectPrinter.tsx
@@ -2,11 +2,14 @@ import * as React from 'react';
 import Project from './Project'
 import './projectStyle.css'
 
-
 export default ()=>{
+    let projectsArray=[
+        {"title":"Test","discription":"this is a good project made here","link":"google.com","pictureRef":"unknown"}
+    ]
+
     return(
         <div>
-           <Project/>
+            <Project title="helo" description="we" link="whatever" pictureRef="ol" />
         </div>
     )
 }
