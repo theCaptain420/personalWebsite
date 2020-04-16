6b6a72b8ba1ef070d1eae0cc2074765163d3f4e7
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-16 17:15:43 +0200

    getting .map to work, apparently its a lot different that RN

--- personal-website/src/Components/welcomeMod/projectElement/ProjectPrinter.tsx
+++ personal-website/src/Components/welcomeMod/projectElement/ProjectPrinter.tsx
@@ -2,14 +2,18 @@ import * as React from 'react';
 import Project from './Project'
 import './projectStyle.css'
 
-export default ()=>{
-    let projectsArray=[
-        {"title":"Test","discription":"this is a good project made here","link":"google.com","pictureRef":"unknown"}
-    ]
+export default () => {
+    const projectsArray = [
+        { "title": "Test", "description": "this is a good project made here", "link": "google.com", "pictureRef": "unknown" }
+    ];
 
-    return(
+    const PrintedListofProjects = projectsArray.map(element => 
+        <Project title={element.title} description={element.description} link={element.link} pictureRef={element.pictureRef} />
+    );
+
+    return (
         <div>
-            <Project title="helo" description="we" link="whatever" pictureRef="ol" />
+            {PrintedListofProjects}
         </div>
-    )
+    );
 }
