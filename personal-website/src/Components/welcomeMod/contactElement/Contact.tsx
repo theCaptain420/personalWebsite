ba142f6784fcd81dbd024b3a0089247fc73dd71a
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-19 20:28:19 +0200

    contact setup

---
+++ personal-website/src/Components/welcomeMod/contactElement/Contact.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+
+
+export default () => {
+    return (
+        <div>
+           <p>
+               Please contact me at +45 28 97 03 12 
+           </p>
+        </div>
+    )
+}
