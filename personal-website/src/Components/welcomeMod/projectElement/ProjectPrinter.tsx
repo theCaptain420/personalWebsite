82c2fb6fea8b9455a0eb49e642d673df7a278de5
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-19 20:38:19 +0200

    ændret lidt fake data

--- personal-website/src/Components/welcomeMod/projectElement/ProjectPrinter.tsx
+++ personal-website/src/Components/welcomeMod/projectElement/ProjectPrinter.tsx
@@ -4,7 +4,7 @@ import './projectStyle.css'
 
 export default () => {
     const projectsArray = [
-        { "title": "Test", "description": "this is a good project made here", "link": "https://github.com/theCaptain420", "pictureRef": "https://images.unsplash.com/photo-1573935146153-f6322e84d1e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" },
+        { "title": "Omkring mig", "description": "Dette afsnit omhandler mig", "link": "https://github.com/theCaptain420", "pictureRef": "https://images.unsplash.com/photo-1573935146153-f6322e84d1e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" },
         { "title": "Test", "description": "this is a good project made here", "link": "https://github.com/theCaptain420", "pictureRef": "https://images.unsplash.com/photo-1573935146153-f6322e84d1e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" }
 
     ];
