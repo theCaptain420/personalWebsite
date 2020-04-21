0cb00c14562384b2c80936990239d7b60c387b66
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-21 11:55:37 +0200

    added contact elemetn

--- personal-website/src/Components/welcomeMod/contactElement/Contact.tsx
+++ personal-website/src/Components/welcomeMod/contactElement/Contact.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
-
+import './contactStyle.css'
 
 
 
 export default () => {
     return (
-        <div>
-           <p>
-               Please contact me at +45 28 97 03 12 
-           </p>
+        <div className="contactBox">
+            <p className="contactTitle">
+                Kontakt mig gerne på en af de følgende måder!
+            </p>
+            <p className="contactText">
+               Telefon: +45 28 97 03 12 <br/>
+               Email: mads.nielsen@hotmail.com
+            </p>
         </div>
     )
 }
