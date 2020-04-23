228bca4a91f612976e5ae696c394b17fa15a3380
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-23 13:40:28 +0200

    made link open in new tab

--- personal-website/src/Components/welcomeMod/contactElement/Contact.tsx
+++ personal-website/src/Components/welcomeMod/contactElement/Contact.tsx
@@ -12,7 +12,7 @@ export default () => {
             <p className="contactText">
                Telefon: +45 28 97 03 12 <br/>
                Email: mads.nielsen@hotmail.com <br/>
-               LinkedIn: <a className="contactText" href='https://www.linkedin.com/in/mads-nielsen-768653110/'> 
+               LinkedIn: <a className="contactText" href='https://www.linkedin.com/in/mads-nielsen-768653110/' target="_blank"> 
                  https://www.linkedin.com/in/mads-nielsen-768653110/
                </a>
             </p>
