9f4bdbb9ff44b8807552a2ff2425d27b7ce1e86f
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-24 11:10:47 +0200

    new footer text

--- personal-website/src/Components/themeComponents/WebFooter.tsx
+++ personal-website/src/Components/themeComponents/WebFooter.tsx
@@ -4,7 +4,7 @@ import './themeStyle.css'
 export default ()=>{
     return(
         <div className="footerBackground">
-            <p className="footerText">made by @thecaptain420</p>
+            <p className="footerText">made by @thecaptain420 in React.JS</p>
         </div>
     )
 }
