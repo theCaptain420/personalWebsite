d465bcc6897236a6f70df6b2326f4ba5e86768a4
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-25 16:38:24 +0200

    compiler fucks up completely with JS files

--- personal-website/src/Components/welcomeMod/githubCount/githubCounter.js
+++ personal-website/src/Components/welcomeMod/githubCount/githubCounter.js
@@ -1,8 +1,29 @@
-import fakeData from './fakecountJson.json'
-export default function countGithubStreak(){
+var fakeData = require('./fakecountJson.json') 
+
+ function countGithubStreak(){
     //https://github-contributions-api.herokuapp.com/thecaptain420/activity
-    for(var propt in fakeData.data){
-        console.log(propt)
+    let daysCommited = 0;
+    let daysNotCommited = 0;
+
+    let dataMonths=fakeData.data[2020];
+
+    for(var month in fakeData.data[2020][month]){
+        console.log(month)
+        for(var day in month[day]){
+            console.log(day)
+            
+        }
     }
+
+
+    Object.keys(dataMonths).forEach(month =>{
+        console.log(month)
+        Object.keys(month).forEach(day={
+            console.log(day)
+        })
+    })
+
+
     return 0;
 }
+countGithubStreak();
