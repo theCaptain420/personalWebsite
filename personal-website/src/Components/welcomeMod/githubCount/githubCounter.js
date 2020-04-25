860fe55f93eb1d3a2e61875e5711dc165d9258ea
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-25 23:06:19 +0200

    moved some code

--- personal-website/src/Components/welcomeMod/githubCount/githubCounter.js
+++ personal-website/src/Components/welcomeMod/githubCount/githubCounter.js
@@ -2,23 +2,11 @@ var fakeData = require('./fakecountJson.json')
 
  function countGithubStreak(){
     //https://github-contributions-api.herokuapp.com/thecaptain420/activity
-    let daysCommited = 1;
-    let daysNotCommited = 0;
-
+    
+  
     let daysInARowCommited = 0;
-    let hasItbeenFound = false;
 
     let dataMonths=fakeData.data[2020];
-    /*
-    Object.keys(dataMonths).forEach(month =>{
-        Object.keys(dataMonths[month]).forEach(day=>{
-            if(dataMonths[month][day]==true){
-                daysCommited+=1;
-            }else{
-                daysNotCommited+=1;
-            }
-        })
-    })*/
 
     let dataToArrayMonth = Object.keys(dataMonths)
     for(let i = dataToArrayMonth.length; i>0; i--){
@@ -37,3 +25,19 @@ var fakeData = require('./fakecountJson.json')
     return 0;
 }
 console.log(countGithubStreak()+ " response");
+
+
+    /*
+
+    let daysCommited = 1;
+    let daysNotCommited = 0;
+
+    Object.keys(dataMonths).forEach(month =>{
+        Object.keys(dataMonths[month]).forEach(day=>{
+            if(dataMonths[month][day]==true){
+                daysCommited+=1;
+            }else{
+                daysNotCommited+=1;
+            }
+        })
+    })*/
