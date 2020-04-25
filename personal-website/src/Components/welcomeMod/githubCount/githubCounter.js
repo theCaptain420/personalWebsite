82268b0713f011c1d36be644460fb9b42c2eee3a
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-25 16:24:19 +0200

    want to try to implement js files instead

--- personal-website/src/Components/welcomeMod/githubCount/githubCounter.js
+++ personal-website/src/Components/welcomeMod/githubCount/githubCounter.js
@@ -1,5 +1,5 @@
 import fakeData from './fakecountJson.json'
-export default function countGithubStreak():number{
+export default function countGithubStreak(){
     //https://github-contributions-api.herokuapp.com/thecaptain420/activity
     for(var propt in fakeData.data){
         console.log(propt)
