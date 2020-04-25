500a859ce88815312a1c418764815abad6d49188
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-25 17:47:53 +0200

    can now count days commited and not commited

--- personal-website/src/Components/welcomeMod/githubCount/githubCounter.js
+++ personal-website/src/Components/welcomeMod/githubCount/githubCounter.js
@@ -2,27 +2,20 @@ var fakeData = require('./fakecountJson.json')
 
  function countGithubStreak(){
     //https://github-contributions-api.herokuapp.com/thecaptain420/activity
-    let daysCommited = 0;
+    let daysCommited = 1;
     let daysNotCommited = 0;
 
     let dataMonths=fakeData.data[2020];
 
-    for(var month in fakeData.data[2020][month]){
-        console.log(month)
-        for(var day in month[day]){
-            console.log(day)
-            
-        }
-    }
-
-
     Object.keys(dataMonths).forEach(month =>{
-        console.log(month)
-        Object.keys(month).forEach(day={
-            console.log(day)
+        Object.keys(dataMonths[month]).forEach(day=>{
+            if(dataMonths[month][day]==true){
+                daysCommited+=1;
+            }else{
+                daysNotCommited+=1;
+            }
         })
     })
-
 
     return 0;
 }
