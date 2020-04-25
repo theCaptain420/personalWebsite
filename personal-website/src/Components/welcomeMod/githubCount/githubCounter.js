87408e954cc7e79762fc1bbffd8d0e6f17cabc23
thecaptain420 <mads.nielsen@hotmail.com>
2020-04-25 23:05:35 +0200

    displays days in a row commited

--- personal-website/src/Components/welcomeMod/githubCount/githubCounter.js
+++ personal-website/src/Components/welcomeMod/githubCount/githubCounter.js
@@ -5,8 +5,11 @@ var fakeData = require('./fakecountJson.json')
     let daysCommited = 1;
     let daysNotCommited = 0;
 
-    let dataMonths=fakeData.data[2020];
+    let daysInARowCommited = 0;
+    let hasItbeenFound = false;
 
+    let dataMonths=fakeData.data[2020];
+    /*
     Object.keys(dataMonths).forEach(month =>{
         Object.keys(dataMonths[month]).forEach(day=>{
             if(dataMonths[month][day]==true){
@@ -15,17 +18,22 @@ var fakeData = require('./fakecountJson.json')
                 daysNotCommited+=1;
             }
         })
-    })
+    })*/
 
     let dataToArrayMonth = Object.keys(dataMonths)
     for(let i = dataToArrayMonth.length; i>0; i--){
         let dataToArrayDay= Object.keys(dataMonths[i])
         for(let k = dataToArrayDay.length; k>0; k--){
             console.log(dataMonths[i][k])
+            if(dataMonths[i][k]==false){
+                return daysInARowCommited;
+            }
+            daysInARowCommited++; 
         }
     }
+
     
 
     return 0;
 }
-countGithubStreak();
+console.log(countGithubStreak()+ " response");
